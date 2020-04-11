85006f3cd4d085f58061457090c9b216791c3133
coder-zzk <2238109333@qq.com>
2020-04-11 22:37:19 +0800

    添加收藏分页查询对象

---
+++ bitbyte-provider-action-8003/src/main/java/com/zzk/bitbyte/po/query/FavoriteQueryVo.java
@@ -0,0 +1,13 @@
+package com.zzk.bitbyte.po.query;
+
+import com.zzk.bitbyte.po.Favorite;
+import lombok.Data;
+
+/**
+ * @Author: zzk
+ * @Date: 2020-04-11 17:17
+ */
+@Data
+public class FavoriteQueryVo extends QueryVo {
+    private Favorite favorite;
+}
